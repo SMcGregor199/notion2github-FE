e99377cba234c5aa15e08dbb435ad1dd7f8b8041
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-10-13 11:58:54 -0400

    add ant design layout,menu,buttons components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import {Header} from "./components/Header";
-import './App.css';
+import {Layout} from "antd";
+import {SiteHeader} from "./components/Header";
+
+
 
 function App() {
-  return (<Header></Header>);
+  return (
+    <Layout>
+      <SiteHeader/>
+      <Layout.Content
+        id="main"
+        style={{ padding: 24, maxWidth: 1200, margin: "0 auto" }}
+      >
+       <h1>Main Content Area</h1> 
+      </Layout.Content>
+    </Layout>
+  );
 }
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,34 @@
-function Header(){
+import { Layout, Menu, Button } from "antd";  
+import { GithubOutlined } from "@ant-design/icons";
+
+function SiteHeader(){
+    const items = [
+        { key: "1", label: <a href="/">Home</a> },
+        { key: "2", label: <a href="/blog">Blog</a> },
+        { key: "3", label: <a href="/login">Login</a> },
+    ];
     return (
     <>
-    <a class="skip-link" href="#main">Skip to main content</a>
-    <header>
-        
-        <div>
-            <a href="/">
-            <svg></svg>
-            <span>Shayne McGregor</span>
-            </a>
+    <a className="skip-link" href="#main">Skip to main content</a>
+    <Layout.Header>
+         <div > 
+            <span style={{ color: "white" }}>Shayne McGregor</span>   
         </div>
-        <nav>
+       <Menu
+            mode="horizontal"
+            items={items}
+       >
+       </Menu>
+        <Button
+            type="default"
+            icon={<GithubOutlined />}
+            href="https://github.com/SMcGregor199"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Open my GitHub profile in a new tab"
+        />
+        
+       {/* <nav>
             <ul>
                 <li><a href="/" aria-current="page">Home</a></li>
                 <li><a href="/blog" aria-current="page">Blog</a></li>
@@ -24,15 +42,15 @@ function Header(){
             rel="noopener noreferrer"
             aria-label="Open my GitHub profile in a new tab"
         >
-        <svg></svg>
-        <span class="sr-only">GitHub</span>
-        </a>
+            <svg></svg>
+            <span className="sr-only">GitHub</span>
+        </a> */}
   
-    </header>
+    </Layout.Header>
     </>
         
     );
 }
 export{
-    Header
+    SiteHeader
 }

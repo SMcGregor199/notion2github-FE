483e0df14d44badc46acb5d646b75afb4ce4ff84
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-10-15 17:26:08 -0400

    add blog card

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,23 @@
-import {ConfigProvider, Layout, theme as antdTheme} from "antd";
+import {Layout, theme as antdTheme} from "antd";
+import { ThemeProvider } from "@emotion/react";
 import SiteHeader from "./components/Header";
 import Hero from "./components/Hero";
+import FeaturedBlogPosts from "./components/FeaturedBlogPosts";
 
 
 
 function App() {
+  const { token } = antdTheme.useToken();
   return (
-    <ConfigProvider
-      theme={{
-        algorithm: antdTheme.defaultAlgorithm,
-        token: {
-          colorPrimary: "#395662",
-          colorBgLayout: "#fff",
-          fontSize: "clamp(0.95rem, 2.8vw, 1.25rem)",
-          fontSizeHeading1: "clamp(1.5rem, 6.5vw, 4.0625rem)",
-          fontSizeHeading2: "clamp(1.25rem, 4vw, 2.25rem)",
-          fontSizeHeading3: "clamp(1.125rem, 3vw, 1.75rem)",
-          fontFamily: "'Noto Sans', system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial, sans-serif",
-          fontFamilyCode: "'Fira Code', ui-monospace, SFMono-Regular, Menlo, Consolas, monospace",
-        },
-        components: {
-          Layout: { headerHeight: 64 },
-        },
-      }}
-    >
+    <ThemeProvider theme={{token}}>
     <Layout>
       <SiteHeader/>
       <Layout.Content style={{ padding: "24px" }} id="main">
         <Hero/>
+        <FeaturedBlogPosts/>
       </Layout.Content>
     </Layout>
-    </ConfigProvider>
+    </ThemeProvider> 
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,26 @@ import { createRoot } from 'react-dom/client'
 import "antd/dist/reset.css";
 import "./index.css";
 import App from './App.jsx'
+import { ConfigProvider, theme as antdTheme } from 'antd';
 
 createRoot(document.getElementById('root')).render(
-<App />
+        <ConfigProvider
+      theme={{
+        algorithm: antdTheme.defaultAlgorithm,
+        token: {
+          colorPrimary: "#D86F44",
+          colorTextLightSolid: "#fff",
+          colorBgLayout: "#fff",
+          fontSize: "clamp(0.95rem, 2.8vw, 1.25rem)",
+          fontSizeHeading1: "clamp(1.5rem, 6.5vw, 4.0625rem)",
+          fontSizeHeading2: "clamp(1.25rem, 4vw, 2.25rem)",
+          fontSizeHeading3: "clamp(1.125rem, 3vw, 1.75rem)",
+          fontFamily: "'Noto Sans', system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial, sans-serif",
+          fontFamilyCode: "'Fira Code', ui-monospace, SFMono-Regular, Menlo, Consolas, monospace",
+        },
+        components: {
+          Layout: { headerHeight: 64 },
+        },
+      }}
+    ><App /></ConfigProvider>
 )

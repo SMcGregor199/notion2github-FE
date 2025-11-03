e4e4c1f214f0f25bacf035a173fb760c0a68fafa
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-11-03 14:50:22 -0500

    …

--- src/components/FeaturedBlogPosts.jsx
+++ src/components/FeaturedBlogPosts.jsx
@@ -1,21 +1,22 @@
 
+import { Typography } from "antd";
 import BlogCardLong from "./BlogCardLong";
 
 
 function FeaturedBlogPosts({initialData}){
-    console.log(initialData);
-    // const blogPosts = initialData.map((post)=>{
-    //     return <BlogCardLong key={post.id} {...post}/>
-    // })
-    // return(
+   
+    const blogPosts = initialData.map((post)=>{
+        return <BlogCardLong key={post.id} {...post}/>
+    })
+    return(
         
-    //     <section>
-    //         <Typography.Title level={2}  style={{ margin: 0, fontWeight: 700, color: "black", paddingBlock: "clamp(24px, 6vh, 48px)",letterSpacing: "-0.02em"}}>
-    //             Featured Blog Posts
-    //         </Typography.Title>
-    //         {blogPosts}
-    //     </section>
-    // )
+        <section>
+            <Typography.Title level={2}  style={{ margin: 0, fontWeight: 700, color: "black", paddingBlock: "clamp(24px, 6vh, 48px)",letterSpacing: "-0.02em"}}>
+                Featured Blog Posts
+            </Typography.Title>
+            {blogPosts}
+        </section>
+    )
 }
 
 export default FeaturedBlogPosts

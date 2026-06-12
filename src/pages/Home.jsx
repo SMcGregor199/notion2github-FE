d72b6a0cc7a958f4df99f5a06f542b97a4be925b
SMcGregor199 <shaynemcgregor1@gmail.com>
2026-06-12 09:36:39 -0400

    feat: add homepage basketball motif

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,10 @@ import Hero from "../components/Hero";
 import FeaturedBlogs from "../components/FeaturedBlogPosts"
 function Home({initialData}) {
     return (
-        <>
+        <div className="homepage-motif">
             <Hero />
             <FeaturedBlogs initialData={initialData}/>
-        </>
+        </div>
     );
 }
 
